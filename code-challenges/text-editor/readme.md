fd1b647580071c5b4f767f88066eae130bdd63d9
Wes Reid <bwreid@gmail.com>
2017-08-02 10:10:16 -0700

    Add Editor code challenge

---
+++ code-challenges/text-editor/readme.md
@@ -0,0 +1,16 @@
+# Editor
+
+Your task is to build an interface for a text editor. The tests will guide you to be able to:
+
+- Read what's been written
+- Write new text
+- Find and replace existing text
+- Undo the previous action
+- Redo the previous action
+
+## Setup
+
+1. Clone this repository
+1. Go to this directory
+1. `yarn` or `npm install`
+1. `yarn test` or `npm test`
---
+++ code-challenges/text-editor/test/editor.test.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const Editor = require('../editor');
+const { expect } = require('chai');
+
+describe('Editor', () => {
+  it('allows users to write text', () => {
+    const editor = new Editor();
+
+    editor.write('Hello - codez');
+    expect(editor.toString()).to.equal('Hello - codez');
+
+    editor.write(' moar');
+    expect(editor.toString()).to.equal('Hello - codez moar');
+  });
+
+  xit('allows users to undo writes', () => {
+    const editor = new Editor();
+
+    editor.write('Hello - codez');
+    expect(editor.toString()).to.equal('Hello - codez');
+
+    editor.write('Moar stuff');
+    expect(editor.toString()).to.equal('Hello - codezMoar stuff');
+
+    editor.write('Even more');
+    expect(editor.toString()).to.equal('Hello - codezMoar stuffEven more');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('Hello - codezMoar stuff');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('Hello - codez');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('');
+  });
+
+  xit('allows users to find and replace', () => {
+    const editor = new Editor();
+
+    editor.write('foo stuff');
+    editor.write(' other foo');
+    editor.replace('foo', 'bar');
+    expect(editor.toString()).to.equal('bar stuff other bar');
+  });
+
+  xit('allows undo replaces', () => {
+    const editor = new Editor();
+
+    editor.write('foo stuff');
+    editor.write(' other foo');
+    editor.replace('foo', 'bar');
+    expect(editor.toString()).to.equal('bar stuff other bar');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('foo stuff other foo');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('foo stuff');
+  });
+
+  xit('allows users to redo', () => {
+    const editor = new Editor();
+
+    editor.write('foo stuff');
+    editor.write(' other foo');
+    editor.replace('foo', 'bar');
+    expect(editor.toString()).to.equal('bar stuff other bar');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('foo stuff other foo');
+
+    editor.undo();
+    expect(editor.toString()).to.equal('foo stuff');
+
+    editor.redo();
+    expect(editor.toString()).to.equal('foo stuff other foo');
+
+    editor.redo();
+    expect(editor.toString()).to.equal('bar stuff other bar');
+  });
+});
